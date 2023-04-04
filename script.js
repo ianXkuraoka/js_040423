4db2c75da60c898d69873ba4a3a2776e86858f7c
Ian <ian.xk9@gmail.com>
2023-04-04 11:17:15 -0300

    incremento, decremento, usando for e usando for como array

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Exemplo de switch
+/*
 let opcao = parseInt(prompt("digite uma opção entre 1 e 3"))
 
 switch(opcao){
@@ -14,9 +16,47 @@ switch(opcao){
         document.write("opção inválida")
         break;
 }
-
-//sssssssssssssssssssssssssssssssssssssssssssssss
+*/
+//Exemplo for
 
 for(let i=0; i <10; i++){
     console.log("Agora i vale "+ i)
+}
+
+//Exemplo "para" com array
+
+let carros =["Gol","Fusca","HB20","corsa"]
+for(let i=0; i<carros.length; i++){
+    console.log(carros[i])
+}
+
+// incremento e decremento
+
+let valor = 7
+document.write(valor)
+document.write("<br><br>")
+valor = valor +=4;
+valor ++
+document.write(valor)
+document.write("<br><br>")
+
+let valor2 = 10
+document.write(valor2)
+valor2 = valor2 -=1
+document.write(valor2)
+document.write("<br><br>")
+
+//usando for
+
+for(let i=0; i<10; i++){
+    document.write("O valor é ", i)
+    document.write("<br><br>")
+}
+
+//usando for como array
+
+let carros2 =["Gol", "Fusca", "Audi", "Corsa"];
+for(let i=0; i <carros.length; i++){
+    document.write(carros2[i])
+    document.write("<br><br>")
 }
